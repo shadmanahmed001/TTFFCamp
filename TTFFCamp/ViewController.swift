e825f86b6854174d6efb61bc8cc0a6edd3830f90
Yanze LIU <yanzeliu.france@gmail.com>
2016-03-09 15:47:54 -0800

    Initial Commit

---
+++ TTFFCamp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTFFCamp
+//
+//  Created by yanze on 3/9/16.
+//  Copyright © 2016 The Taylor Family Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
